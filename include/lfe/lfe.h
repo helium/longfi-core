09d282becaac2a0bea567e7133623645ddea7038
Jay Kickliter <jay@kickliter.com>
2019-09-30 10:20:57 -0700

    lfe: fill in some missing API

--- include/lfe/lfe.h
+++ include/lfe/lfe.h
@@ -1,22 +1,80 @@
 #ifndef LFE_0ED159C6
 #define LFE_0ED159C6
 
+/**
+ * @file
+ */
+
+#include <stddef.h>
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+
 /**
- * @brief LongFI engine context.
+ * LongFi regulatory region.
+ *
+ * LongFi users are responsible for configuring their devices' region
+ * in order to remain compliant with regulations.
+ */
+enum lfe_region {
+    /** USA */
+    lfe_region_fcc,
+};
+
+/**
+ * LongFi engine user configuration.
+ */
+struct lfe_user_cfg {};
+
+/**
+ * LongFi engine context.
  *
  * More
  */
-struct lfe {};
+struct lfe {
+    /** User-provided configuration. */
+    struct lfe_user_cfg cfg;
+};
 
 /**
- * @brief Initializes a user-provided `lfe` object.
+ * LoRa spreading factor.
+ *
+ * Low spreading factors spend less time on air but are more
+ * susceptible to errors, where as large spreading factors are more
+ * tolerant to noise but are slow to transmit.
+ */
+enum sf {
+    sf_5  = 5,
+    sf_6  = 6,
+    sf_7  = 7,
+    sf_8  = 8,
+    sf_9  = 9,
+    sf_10 = 10,
+    sf_11 = 11,
+    sf_12 = 12,
+};
+
+/**
+ * Initializes a user-provided `lfe` object.
  */
 void
 lfe_init(struct lfe * lfe);
+
+/**
+ * Push a received payload into the engine.
+ */
+void
+lfe_push_rx(struct lfe * lfe, uint8_t const * pay, size_t pay_len);
+
+/**
+ * Initializes and returns a transmit plan.
+ */
+void
+lfe_transmit(struct lfe * lfe, uint8_t const * pay, size_t pay_len);
+
 
 #ifdef __cplusplus
 }
--- src/lfe.c
+++ src/lfe.c
@@ -1,2 +1,14 @@
+#include <assert.h>
 #include <golay/golay.h>
 #include <lfe/lfe.h>
+
+void
+lfe_init(struct lfe * lfe) {
+    assert(0);
+}
+
+
+void
+lfe_push_rx(struct lfe * lfe, uint8_t const * pay, size_t pay_len) {
+    assert(0);
+}
